7329231dc4aa1a8f8cebf22aefa797ec7be460ad
James Smead <jsmead39@yahoo.com>
2023-04-12 11:49:14 -0500

    Builder for workoutRequest

--- MusicPlaylistServiceLambda/src/main/java/com/nashss/se/musicplaylistservice/activity/requests/CreateWorkoutRequest.java
+++ MusicPlaylistServiceLambda/src/main/java/com/nashss/se/musicplaylistservice/activity/requests/CreateWorkoutRequest.java
@@ -1,15 +1,16 @@
 package com.nashss.se.musicplaylistservice.activity.requests;
 
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
 
 @JsonDeserialize(builder = CreatePlaylistRequest.Builder.class)
 public class CreateWorkoutRequest {
     private final String customerId;
     private final String date;
     private final String workoutType;
-    private Integer durationInHours;
-    private Integer durationInMinutes;
-    private Integer durationInSeconds;
+    private final Integer durationInHours;
+    private final Integer durationInMinutes;
+    private final Integer durationInSeconds;
     private final Double distance;
 
     public CreateWorkoutRequest(String customerId, String date, String workoutType, Integer durationInHours,
@@ -62,5 +63,61 @@ public class CreateWorkoutRequest {
                 ", durationInSeconds=" + durationInSeconds +
                 ", distance=" + distance +
                 '}';
+    }
+
+    //CHECKSTYLE:OFF:Builder
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    @JsonPOJOBuilder
+    public static class Builder {
+        private String customerId;
+        private String date;
+        private String workoutType;
+        private Integer durationInHours;
+        private Integer durationInMinutes;
+        private Integer durationInSeconds;
+        private Double distance;
+
+
+        public Builder withCustomerId(String customerId) {
+            this.customerId = customerId;
+            return this;
+        }
+
+        public Builder withDate(String date) {
+            this.date = date;
+            return this;
+        }
+
+        public Builder withWorkoutType(String workoutType) {
+            this.workoutType = workoutType;
+            return this;
+        }
+
+        public Builder withDurationInHours(Integer durationInHours) {
+            this.durationInHours = durationInHours;
+            return this;
+        }
+
+        public Builder withDurationInMinutes(Integer durationInMinutes) {
+            this.durationInMinutes = durationInMinutes;
+            return this;
+        }
+
+        public Builder withDurationInSeconds(Integer durationInSeconds) {
+            this.durationInSeconds = durationInSeconds;
+        }
+
+        public Builder withDistance(Double distance) {
+            this.distance = distance;
+            return this;
+        }
+
+        public CreateWorkoutRequest build() {
+            return new CreateWorkoutRequest(customerId, date, workoutType, durationInHours, durationInMinutes,
+                    durationInSeconds, distance);
+        }
     }
 }
