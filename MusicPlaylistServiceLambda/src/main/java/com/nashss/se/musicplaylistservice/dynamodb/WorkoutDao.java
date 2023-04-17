e99e1e41c709885b132c67213d57f444ebadcd4e
Aaron Kaye <kaye.aaron@gmail.com>
2023-04-17 15:42:11 -0400

    Implemented WorkoutDao getAllTriathlonRecordsByCustomer method

--- MusicPlaylistServiceLambda/src/main/java/com/nashss/se/musicplaylistservice/dynamodb/WorkoutDao.java
+++ MusicPlaylistServiceLambda/src/main/java/com/nashss/se/musicplaylistservice/dynamodb/WorkoutDao.java
@@ -1,16 +1,23 @@
 package com.nashss.se.musicplaylistservice.dynamodb;
 
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBQueryExpression;
+import com.amazonaws.services.dynamodbv2.datamodeling.PaginatedQueryList;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
 import com.nashss.se.musicplaylistservice.dynamodb.models.Triathlon;
 import com.nashss.se.musicplaylistservice.metrics.MetricsPublisher;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Singleton
 public class WorkoutDao {
     private final DynamoDBMapper dynamoDbMapper;
     private final MetricsPublisher metricsPublisher;
+    private final String CUSTOMER_ID_INDEX = "CustomerIdIndex";
 
     /**
      * Instantiates a WorkoutDao object.
@@ -32,6 +39,18 @@ public class WorkoutDao {
      */
     public Triathlon getTriathlon(String workoutId) {
         return this.dynamoDbMapper.load(Triathlon.class, workoutId);
+    }
+
+    public List<Triathlon> getAllTriathlonRecordsByCustomer(String customerId) {
+        Map<String, AttributeValue> valueMap = new HashMap<>();
+        valueMap.put(":customerId", new AttributeValue().withS(customerId));
+        DynamoDBQueryExpression<Triathlon> queryExpression = new DynamoDBQueryExpression<Triathlon>()
+                .withIndexName(CUSTOMER_ID_INDEX)
+                .withConsistentRead(false)
+                .withKeyConditionExpression("customerId = :customerId")
+                .withExpressionAttributeValues(valueMap);
+
+        return dynamoDbMapper.query(Triathlon.class, queryExpression);
     }
 
     /**
